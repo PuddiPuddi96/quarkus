7a72791ffa953ba33e0e6f7b637411b5eea6c360
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-09-14 16:06:35 +0200

    dev: implement rest api to test simple query

--- quarkus-jpa/src/main/java/it/davide/course/quarkus/FilmResource.java
+++ quarkus-jpa/src/main/java/it/davide/course/quarkus/FilmResource.java
@@ -1,12 +1,21 @@
 package it.davide.course.quarkus;
 
+import it.davide.course.quarkus.model.Film;
+import it.davide.course.quarkus.repository.FilmRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Path("/")
 public class FilmResource {
+
+    @Inject
+    FilmRepository filmRepository;
 
     @GET
     @Path("/helloWorld")
@@ -16,10 +25,43 @@ public class FilmResource {
     }
 
     @GET
-    @Path("/helloWorld2")
+    @Path("/film/{filmId}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello2(){
-        return "Hello world2";
+    public String getFilm(short filmId){
+        Optional<Film> film = filmRepository.getFilm(filmId);
+        return film.isPresent() ? film.get().getTitle() : "No film was found!";
     }
+
+    @GET
+    @Path("/pagedFilm/{page}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String paged(long page, short minLength){
+        return filmRepository.paged(page, minLength)
+                .map(film ->
+                        String.format("%s (%d min)",
+                                film.getTitle(),
+                                film.getLength()))
+                .collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/actors/{startsWith}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String actors(String startsWith, short minLength){
+        return filmRepository.actors(startsWith, minLength)
+                .map(film ->
+                        String.format("%s (%d min): %s",
+                                film.getTitle(),
+                                film.getLength(),
+                                film.getActors().stream()
+                                        .map(actor ->
+                                            String.format("%s %s",
+                                                actor.getFirstName(),
+                                                actor.getLastName()))
+                                        .collect(Collectors.joining(", "))))
+                .collect(Collectors.joining("\n"));
+    }
+
+
     
 }
---
+++ quarkus-jpa/src/main/java/it/davide/course/quarkus/repository/FilmRepository.java
@@ -0,0 +1,44 @@
+package it.davide.course.quarkus.repository;
+
+import com.speedment.jpastreamer.application.JPAStreamer;
+import com.speedment.jpastreamer.projection.Projection;
+import com.speedment.jpastreamer.streamconfiguration.StreamConfiguration;
+import it.davide.course.quarkus.model.Film;
+import it.davide.course.quarkus.model.Film$;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@ApplicationScoped
+public class FilmRepository {
+
+    @Inject
+    JPAStreamer jpaStreamer;
+
+    private static final int PAGE_SIZE = 20;
+
+    public Optional<Film> getFilm(short filmId) {
+        return jpaStreamer.stream(Film.class)
+                .filter(Film$.filmId.equal(filmId))
+                .findFirst();
+    }
+
+    public Stream<Film> paged(long page, short minLength) {
+        //We need a constructor with tihis three fields
+        return jpaStreamer.stream(Projection.select(Film$.filmId, Film$.title, Film$.length))
+                .filter(Film$.length.greaterThan(minLength))
+                .sorted(Film$.length)
+                .skip(page * PAGE_SIZE)
+                .limit(PAGE_SIZE);
+    }
+
+    public Stream<Film> actors(String startsWith, short minLength) {
+        final StreamConfiguration<Film> sc =
+                StreamConfiguration.of(Film.class).joining(Film$.actors);
+        return jpaStreamer.stream(sc)
+                .filter(Film$.title.startsWith(startsWith).and(Film$.length.greaterThan(minLength)))
+                .sorted(Film$.length.reversed());
+    }
+}
