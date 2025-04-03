f3ff8c69650563d52f3701d39b2e82d1fb2e6ecd
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-04-03 17:42:41 +0200

    [Q][CONF]: Set-up project

---
+++ rest-book/src/test/java/it/davide/quarkus/starting/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package it.davide.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
