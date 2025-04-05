396017cbcca4757e4d24c7b6b2532f6fb5775144
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-04-05 11:34:09 +0200

    [Q][FEAT]: add Book repository

--- rest-book/src/main/java/it/davide/quarkus/starting/BookRepository.java
+++ rest-book/src/main/java/it/davide/quarkus/starting/BookRepository.java
@@ -1,2 +1,22 @@
-package it.davide.quarkus.starting;public class BookRepository {
+package it.davide.quarkus.starting;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class BookRepository {
+
+    public List<Book> getAllBooks(){
+        return List.of(
+                new Book(1, "Title one", "Davide", 2025, "IT"),
+                new Book(2, "Title two", "Luca", 2024, "IT"),
+                new Book(3, "Title three", "Gennaro", 2023, "IT")
+        );
+    }
+
+    public Optional<Book> getBook(int id){
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    }
 }
