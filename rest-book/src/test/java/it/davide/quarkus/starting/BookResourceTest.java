1d0a814756d190b0d1d260bef1b31c32ee9623b4
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-04-07 12:01:23 +0200

    [Q][REF]: Deleted some files

---
+++ rest-book/src/test/java/it/davide/quarkus/starting/BookResourceTest.java
@@ -0,0 +1,25 @@
+package it.davide.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+class BookResourceTest {
+
+    @Test
+    void shouldGetAllBooks(){
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when()
+                .get("/api/books")
+                .then()
+                .statusCode(200)
+                .body("size()", is(3));
+    }
+}
